bf47fa8d1ddddc0ddb56f93c7b089eb34a131d86
silc22 <you@example.com>
2022-10-17 13:29:28 +0200

    [FT] estilos en pagina Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,65 @@
-import Navbar from "../components/Navbar"
-import Footer from "../components/Footer"
-
+import { useState, React } from "react"
+import { Link } from "react-router-dom"
+import "../styles/home.css"
 
 function Home() {
+  const [newUser, setNewUser] = useState ({name: ""})
+  
+  
+  const inputHandler = (e) => {
+    setNewUser({
+        [e.target.name]: e.target.value
+    })
+  }
+
+  const submitForm = (e) => {
+    e.preventDefault()
+    if (newUser.name === "" || newUser.name.length > 10 || newUser.name.length < 3){
+    alert('There are fields incomplete, please complete them.') 
+    }
+}
 
    return (
     <>
-      <Navbar/>
-        <main>
+        <main className="home">
+
+          <div className="home__container">
+
+            <h2 className="home__title">
+              Qué <span>bueno</span> verte!
+            </h2>
+
+            <form className="home_form">
+
+              <label 
+                htmlFor="name"
+                className="home__label">
+                  ¿Cuál es tu nombre?
+              </label>
+              <input 
+              className="home__input"
+              type="text" 
+              id="name" 
+              name="name" 
+              onChange={inputHandler}/>
+
+              <Link 
+              className="home__link"
+              to="/" 
+              onClick={(e) => submitForm(e)}>
+
+                <button 
+                className="home__button"
+                type="submit">
+                  Comenzar
+                </button>
+
+              </Link>
+            </form>
+
+          </div>
         
         </main>
-      <Footer/>
     </>
     );
   }
