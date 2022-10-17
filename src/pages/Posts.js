ce30de348c458852b7155b7f0ebae99c5d2b0cc9
silc22 <you@example.com>
2022-10-17 16:12:31 +0200

    [FX]Estilos para footer,posts y globales

--- src/pages/Posts.js
+++ src/pages/Posts.js
@@ -8,32 +8,31 @@ function Posts() {
    const {cards} = useSelector((state)=> state.post)
    const dispatch = useDispatch()
 
-   
-
 
    useEffect(()=>{
       dispatch(getAllPosts())
    },[dispatch])
-   
-
   
    
     return (
      <section className="seccion__posts">
       
-      <div className="seccion__indivualCard">
-        <Post />
-      </div>
+         <div className="seccion__indivualCard">
+         <Post />
+         </div>
         
         <div className="seccion__cards">
             {cards.map((card)=>(
-               <div key={card.id} className="seccion__card" onClick={()=>dispatch(showOneCard(card.id))}>
-                  <p>
-                     {card.userId}
-                  </p>
+               <div key={card.id} className="seccion__card" >
                   <p>
                      {card.title}
                   </p>
+                  <p>
+                     {card.userId}
+                  </p>
+                  <button onClick={()=>dispatch(showOneCard(card.id))}>
+                     ver mas
+                  </button>
                </div>
             ))}
         </div>
