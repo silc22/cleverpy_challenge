51b9ff2be014a6e13671f5a0492ede23bcad8ca4
silc22 <you@example.com>
2022-10-17 22:35:27 +0200

    [FT] Creacion de componente Card y estilo

---
+++ src/components/Card.js
@@ -0,0 +1,26 @@
+import "../styles/card.css"
+import { deleteCard } from "../store/actions/posts/postsActions"
+import { useDispatch, useSelector } from "react-redux";
+
+function Card() {
+    const dispatch = useDispatch()
+    const {oneCard} = useSelector((state)=> state.post)
+   
+    return (    
+        <>
+        {oneCard.map((element)=>(
+           <article className="card" key={element.id}>
+                <p>{element.userId}</p>    
+                <p>{element.title}</p>    
+                <p>{element.body}</p>    
+                <button onClick={()=>dispatch(deleteCard(element.id))}>
+                eliminar card
+                </button>
+            </article>
+        ))}
+      </>
+     );
+   }
+   
+   export default Card;
+   
