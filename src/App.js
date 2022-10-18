a97b9919c1742106ce33829d0a2129250e8b8404
silc22 <you@example.com>
2022-10-18 23:12:36 +0200

    [FX] Modificando componentes en rutas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom"
-import Navbar from "./components/Navbar";
 import Home from "./pages/Home"
 import Posts from "./pages/Posts"
-import Footer from "./components/Footer";
 
 //Redux
 import { Provider } from 'react-redux';
@@ -10,15 +8,18 @@ import store from './store/store';
 
 
 function App() {
+  
+
   return (
     <BrowserRouter>
       <Provider store={store}>
-        <Navbar/>
+        
         <Routes>
           <Route path="/" element={<Home/>}/>
+          <Route path="*" element={<Home/>}/>
           <Route path="/posts" element={<Posts/>}/>
         </Routes>
-        <Footer/>
+
       </Provider>
     </BrowserRouter>
   );
