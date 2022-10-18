0a9a9dc7973471b7bd9a687d9c0a542a21530b2b
silc22 <you@example.com>
2022-10-18 23:09:26 +0200

    [FX] moficando estilo y componente

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "../styles/sidebar.css"
 
 function Sidebar() {
@@ -5,17 +6,38 @@ function Sidebar() {
    
     return (    
       <aside className="sidebar">
-        <ul className="sidebar__menu">
-          <li className="sidebar__icons">
-              Telegram
-          </li>
-          <li className="sidebar__icons">
-              Whatsapp
-          </li>
-          <li className="sidebar__icons">
-              Github
-          </li>
-        </ul>
+        <ul className="sidebar__list">
+            <li className="sidebar__item">
+              <Link to="/" className="sidebar__link">
+                Home
+              </Link>
+            </li>
+            <li className="sidebar__item">
+              <Link to="/posts" className="sidebar__link">
+                Explora
+              </Link>
+            </li>
+            <li className="sidebar__item">
+              <Link to="/posts" className="sidebar__link">
+                Notificaciones
+              </Link>
+            </li>
+            <li className="sidebar__item">
+              <Link to="/posts" className="sidebar__link">
+                Mensajes
+              </Link>
+            </li>
+            <li className="sidebar__item">
+              <Link to="/posts" className="sidebar__link">
+                Amigos
+              </Link>
+            </li>
+            <li className="sidebar__item">
+              <Link to="/posts" className="sidebar__link">
+                Favoritos
+              </Link>
+            </li>
+          </ul>
         
       </aside>
      );
