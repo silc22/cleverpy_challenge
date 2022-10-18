8d0752a1555069795a0dea74b5b0b2dde333e8cf
silc22 <you@example.com>
2022-10-18 23:11:57 +0200

    [FX] Modif y dando estilos

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,69 +1,30 @@
-import { useState, React } from "react"
-import { Link, useNavigate } from "react-router-dom"
+import { React } from "react"
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import Form from "../components/Form";
 import "../styles/home.css"
 
 
 function Home() {
-  const [newUser, setNewUser] = useState ({name: ""})
-  const navigate = useNavigate()
-  
-  const inputHandler = (e) => {
-    setNewUser({
-        [e.target.name]: e.target.value
-    })
-  }
-
-  const submitForm = (e) => {
-    e.preventDefault()
-    if (newUser.name === "" || newUser.name.length > 10 || newUser.name.length < 3){
-    alert('There are fields incomplete, please complete them.') 
-    }else{
-      localStorage.setItem("users", JSON.stringify(newUser))
-      navigate("/posts")
-    }
-}
+ 
 
    return (
-    <>
-        <main className="home">
+    <>  
+      <Navbar/>
+      <main className="home">
 
-          <div className="home__container">
+        <div className="home__container">
 
-            <h2 className="home__title">
-              Qué <span>bueno</span> verte!
-            </h2>
+          <h2 className="home__title">
+            Qué <span>bueno</span> verte!
+          </h2>
 
-            <form className="home_form">
+          <Form/>
 
-              <label 
-                htmlFor="name"
-                className="home__label">
-                  ¿Cuál es tu nombre?
-              </label>
-              <input 
-              className="home__input"
-              type="text" 
-              id="name" 
-              name="name" 
-              onChange={(e)=>inputHandler(e)}/>
-
-              <Link 
-              className="home__link"
-              to="/" 
-              onClick={(e) => submitForm(e)}>
-
-                <button 
-                className="home__button"
-                type="submit">
-                  Comenzar
-                </button>
-
-              </Link>
-            </form>
-
-          </div>
-        
-        </main>
+        </div>
+      
+      </main>
+      <Footer/>
     </>
     );
   }
